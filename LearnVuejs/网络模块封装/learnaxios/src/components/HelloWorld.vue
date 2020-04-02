48e77cb80a490fe8bc1476ca47afaf868544c162
Iamanuan <gemma.mobu@icloud.com>
2020-04-02 10:33:12 +0800

    axios网络封装

---
+++ LearnVuejs/网络模块封装/learnaxios/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>{{categories}}</h2>
+</template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    name: "HelloWorld",
+    data(){
+      return{
+        categories: ''
+      }
+    },
+    created() {
+      axios({
+        url: 'http://152.136.185.210:8000/api/h3/category'
+      }).then(res => {
+        this.categories = res
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
