b08c80f429fba3086e8fb947e1f255161023453f
Iamanuan <gemma.mobu@icloud.com>
2020-04-04 21:36:31 +0800

    supermall Day1

---
+++ supermall/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//1、安装插件
+Vue.use(VueRouter)
+
+const Home = () => import('views/home/Home')
+const Category = () => import('views/category/Category')
+const Profile = () => import('views/profile/Profile')
+const Shopcar = () => import('views/shopcar/Shopcar')
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    path: '/shopcar',
+    component: Shopcar
+  }
+]
+
+//2、创建router
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+export default router
